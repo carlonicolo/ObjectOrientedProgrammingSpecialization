5ed579312d65706d2dea0931609180c011ca0a82
Carlo <carlo.nicolo@hotmail.com>
2024-06-19 14:27:52 +0200

    Fix

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<string> tokenize(string csvLine, char separator)
+{
+    vector<string> tokens;
+    return tokens;
+}
+
+int main()
+{
+    
+    return 0;
+}
